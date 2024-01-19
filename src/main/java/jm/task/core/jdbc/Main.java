b1b2d7961496d554d7162578ae2d7fc4051af197
future <tebenkova100@icloud.com>
2024-01-19 20:51:42 +0800

    Добавить решение для задачи 1.1.3

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,27 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
+
+    private final static UserService userService = new UserServiceImpl();
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+
+        userService.createUsersTable();
+
+        userService.saveUser("Anna", "Tebenkova", (byte) 33);
+        userService.saveUser("Lilly", "Tebenkova", (byte) 4);
+        userService.saveUser("Margarida", "Dos Maritz", (byte) 35);
+        userService.saveUser("Adriano", "Santos", (byte) 40);
+
+        userService.removeUserById(2);
+
+        userService.getAllUsers();
+
+        userService.cleanUsersTable();
+
+        userService.dropUsersTable();
     }
 }
